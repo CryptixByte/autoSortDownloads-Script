8d8bcd3d35a7a706c72676cd5ee9fc56ad22f646
Marc S. <168722619+CryptixByte@users.noreply.github.com>
2025-10-03 19:09:37 -0700

    Update aSD.bat

--- aSD.bat
+++ aSD.bat
@@ -2,42 +2,42 @@
 
 cd %USERPROFILE%\Downloads
 
-echo Handling Documents in Downloads
-move "*.docx" "%USERPROFILE%\Documents"
-move "*.odt" "%USERPROFILE%\Documents"
-move "*.pdf" "%USERPROFILE%\Documents"
-move ".pptx" "%USERPROFILE%\Documents"
-move "*.rtf" "%USERPROFILE%\Documents"
-move "*.txt" "%USERPROFILE%\Documents"
-move "*.xlsx" "%USERPROFILE%\Documents"
+@echo Handling Documents in Downloads
+move "*.docx" "%USERPROFILE%\Documents" >nul 2>&1
+move "*.odt" "%USERPROFILE%\Documents" >nul 2>&1
+move "*.pdf" "%USERPROFILE%\Documents" >nul 2>&1
+move ".pptx" "%USERPROFILE%\Documents" >nul 2>&1
+move "*.rtf" "%USERPROFILE%\Documents" >nul 2>&1
+move "*.txt" "%USERPROFILE%\Documents" >nul 2>&1
+move "*.xlsx" "%USERPROFILE%\Documents" >nul 2>&1
 
 pause
 
-echo Handling Images/Pictures in Downloads
-move "*.bmp" "%USERPROFILE%\Pictures"
-move "*.gif" "%USERPROFILE%\Pictures"
-move "*.jpeg" "%USERPROFILE%\Pictures"
-move "*.jpg" "%USERPROFILE%\Pictures"
-move "*.png" "%USERPROFILE%\Pictures"
-move "*.webp" "%USERPROFILE%\Pictures"
+@echo Handling Images/Pictures in Downloads
+move "*.bmp" "%USERPROFILE%\Pictures" >nul 2>&1
+move "*.gif" "%USERPROFILE%\Pictures" >nul 2>&1
+move "*.jpeg" "%USERPROFILE%\Pictures" >nul 2>&1
+move "*.jpg" "%USERPROFILE%\Pictures" >nul 2>&1
+move "*.png" "%USERPROFILE%\Pictures" >nul 2>&1
+move "*.webp" "%USERPROFILE%\Pictures" >nul 2>&1
 
 pause 
 
-echo Handling Audio/Music in Downloads
-move "*.aac" "%USERPROFILE%\Music"
-move "*.m4a" "%USERPROFILE%\Music"
-move "*.mp3" "%USERPROFILE%\Music"
-move "*.wav" "%USERPROFILE%\Music"
+@echo Handling Audio/Music in Downloads
+move "*.aac" "%USERPROFILE%\Music" >nul 2>&1
+move "*.m4a" "%USERPROFILE%\Music" >nul 2>&1
+move "*.mp3" "%USERPROFILE%\Music" >nul 2>&1
+move "*.wav" "%USERPROFILE%\Music" >nul 2>&1
 
 pause
 
-echo Handling Videos in Downloads
-move "*.avi" "%USERPROFILE%\Videos"
-move "*.flv" "%USERPROFILE%\Videos"
-move "*.mov" "%USERPROFILE%\Videos"
-move "*.mp4" "%USERPROFILE%\Videos"
-move "*.mkv" "%USERPROFILE%\Videos"
-move "*.flv" "%USERPROFILE%\Videos"
+@echo Handling Videos in Downloads
+move "*.avi" "%USERPROFILE%\Videos" >nul 2>&1
+move "*.flv" "%USERPROFILE%\Videos" >nul 2>&1
+move "*.mov" "%USERPROFILE%\Videos" >nul 2>&1
+move "*.mp4" "%USERPROFILE%\Videos" >nul 2>&1
+move "*.mkv" "%USERPROFILE%\Videos" >nul 2>&1
+move "*.flv" "%USERPROFILE%\Videos" >nul 2>&1
 
 echo Done
 
